ab5f8fb5a77cc5eb5de2a27f9d5c8ad432d0a38e
alamsamudrow <76034571+alamsamudrow@users.noreply.github.com>
2020-12-15 14:37:14 +0700

    Add files via upload

---
+++ data/ANANG_1.js
@@ -0,0 +1,8 @@
+var json_ANANG_1 = {
+"type": "FeatureCollection",
+"name": "ANANG_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.361071928978788, -7.778406083589582 ] } }
+]
+}
